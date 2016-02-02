312d3692bf3b9e2e9f7d2488487247d84d470e8a
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2016-02-02 15:15:54 +0200

    Changed backend.

--- lib/delayed_job_couchrest_model.rb
+++ lib/delayed_job_couchrest_model.rb
@@ -2,5 +2,6 @@ require 'couchrest_model'
 require 'delayed_job'
 require 'delayed/serialization/couchrest_model'
 require 'delayed/backend/couchrest_model'
-Delayed::Worker.backend = Delayed::Backend::CouchrestModel::Job
-Delayed::Backend::CouchrestModel::Job.use_database ::CouchRest.database('delayed_job')
+# Delayed::Worker.backend = Delayed::Backend::CouchrestModel::Job
+# Delayed::Backend::CouchrestModel::Job.use_database ::CouchRest.database('delayed_job')
+Delayed::Worker.backend = :couchrest_model
